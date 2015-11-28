b24fa7fe17978733c1c700b511ca34e2cd71fe46
Atanas Penchev <atanas.penchev@outlook.com>
2015-11-28 00:38:05 +0000

    Criterias submit.

--- criteria.js
+++ criteria.js
@@ -1,6 +1,6 @@
 (function($) {
 
-  $('.selectable').on('click', function(event) {
+  $('.selectable').click(function(event) {
     event.preventDefault();
 
     var $link = $(event.target);
@@ -13,6 +13,34 @@
     value = value.split('&lt;').join('<'); // Unescape HTML character
 
     $('#' + targetId).val(value);
+  });
+
+  $('#submit').click(function(event) {
+    event.preventDefault();
+
+    var values = [];
+    var areAllInputesFilled = true;
+
+    $('#form').find('input').each(function(i, el) {
+      var $this = $(this);
+      var id = $this.attr('id');
+      
+      var key = id.split('_value').join('');
+      var value = $this.val().trim();
+
+      if (!value) areAllInputesFilled = false;
+
+      values.push(key + '=' + value);
+    });
+
+    if (!areAllInputesFilled) {
+      alert('Please, fill all inputs.');
+      return;
+    }
+
+    var url = 'results.html?';
+    url += values.join('&');
+    window.location = url;
   });
 
 })(jQuery);
